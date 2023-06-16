9e08d7b790f2a6d1934521afcca71c51294097fb
MaksymW <mwerema@concisesoftware.com>
2023-06-16 13:24:01 +0200

    1112

--- src/GPT.ts
+++ src/GPT.ts
@@ -1,42 +1,5 @@
 import axios from 'axios';
 
-
-// export async function getGpt3Response(message:string,OPENAI_API_KEY:string) {
-//   const apiUrl = 'https://api.openai.com/v1/chat/completions';
-
-//   const requestBody = {
-//     model: 'gpt-3.5-turbo', 
-//     messages: [
-//         { role: 'system', content: 'You are a helpful assistant.' },
-//         { role: 'user', content: message }
-//     ],
-// };
-
-//   const requestConfig = {
-//       headers: {
-//           'Content-Type': 'application/json',
-//           Authorization: `Bearer ${OPENAI_API_KEY}`,
-//       },
-//   };
- 
-//   try {
-//       const response = await axios.post(apiUrl, requestBody, requestConfig);
-//       const responseData = response.data;
-
-//       if (responseData.choices && responseData.choices.length > 0) {
-//           const chatResponse = responseData.choices[0].message.content;
-//           return chatResponse;
-//       } else {
-//           throw new Error('No chat response received');
-//       }
-//   } catch (error) {
-//       console.error('Error retrieving chat response:', error);
-//       throw error;
-//   }
-// }
-
-
-
 export class ChatGPTHelper {
   private apiKey: string;
   private apiUrl: string;
@@ -48,7 +11,6 @@ export class ChatGPTHelper {
 
   public async getChatResponse(message: string): Promise<string> {
     try {
-
 
   const requestBody = {
     model: 'gpt-3.5-turbo', 
